e6a9f23dbd71298b0d30031e574249af15504e8f
Tmac_Creates <tmac_1990@outlook.com>
2026-04-06 23:58:02 -0400

    [Cre.] Create a new "Flexbox" component.
    I created a new "Flexbox" component and added all the basics to get routing setup along with HTML and CSS.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Home } from './home/home';
 import { About } from './about/about';
 import { Tutorial } from './tutorial/tutorial';
 import { MediaQueries } from './media-queries/media-queries';
+import { Flexbox } from './flexbox/flexbox';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'tutorial', component: Tutorial },
-  {path: 'media-queries', component: MediaQueries}
+  { path: 'media-queries', component: MediaQueries},
+  { path: 'flexbox', component: Flexbox},
 ];

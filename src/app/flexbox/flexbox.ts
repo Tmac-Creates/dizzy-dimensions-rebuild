e764402ce1a5708a7314761a34b4e8b3c26ba705
Tmac_Creates <tmac_1990@outlook.com>
2026-04-06 23:58:02 -0400

    [Cre.] Create a new "Flexbox" component.
    I created a new "Flexbox" component and added all the basics to get routing setup along with HTML and CSS.

---
+++ src/app/flexbox/flexbox.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flexbox',
+  imports: [],
+  templateUrl: './flexbox.html',
+  styleUrl: './flexbox.css',
+})
+export class Flexbox {}
